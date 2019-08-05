79c616cdde494378dec25209c467af5bbdc2e55c
Siraj Umeya <din.siraj7@gmail.com>
2019-08-05 21:51:29 +0000

    converting downloaded image using the image tag

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,14 +3,14 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.comments.create(comment_params.merge(user: current_user))
+    # @place.comments.create(comment_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:photo).permit(:caption)
+    params.require(:photo).permit(:caption, :picture)
   end
 
 end
